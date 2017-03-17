cb03d57e005a71aca73766e4d91bf0fc264e5db2
imesong <imesong@126.com>
2017-03-17 09:50:55 +0800

    更新roles 表字段

--- app/models.py
+++ app/models.py
@@ -9,10 +9,43 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permissions.FOLLOW |
+                     Permissions.COMMENT |
+                     Permissions.WRITE_ARTICLES, True),
+            'Moderator': (Permissions.FOLLOW |
+                          Permissions.COMMENT |
+                          Permissions.WRITE_ARTICLES |
+                          Permissions.MODIFY_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+
+class Permissions:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODIFY_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
 
 
 class User(UserMixin, db.Model):
