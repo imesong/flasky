db5fa6dd47d31436445a34e061afaf5ef5f0c462
imesong <imesong@126.com>
2017-03-18 22:09:21 +0800

    完成分页显示，底部导航条

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'imesong@126.com'
-
+    FLASKY_POSTS_PER_PAGE = os.environ.get('FLASKY_POSTS_PER_PAGE') or 20
     @staticmethod
     def init_app(app):
         pass
