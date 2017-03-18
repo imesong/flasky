46c30022bae08f846583a88e9a0d8b9a05d4b29a
imesong <imesong@126.com>
2017-03-18 22:57:58 +0800

    完成博客固定连接

--- app/main/views.py
+++ app/main/views.py
@@ -21,9 +21,7 @@ def index():
         error_out=False
     )
     posts = pagination.items
-    return render_template('index.html',
-                           form=form,
-                           posts=posts,
+    return render_template('index.html', form=form, posts=posts,
                            pagination=pagination)
 
 
@@ -81,3 +79,8 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
