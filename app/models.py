608d5e79aae5c9c3b532b631543d8755addfe65f
imesong <imesong@126.com>
2017-03-18 22:33:09 +0800

    添加服务的markdown 支持

--- app/models.py
+++ app/models.py
@@ -4,7 +4,8 @@ from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from flask_moment import datetime
-import hashlib
+import hashlib, bleach
+from markdown import markdown
 
 
 class Role(db.Model):
@@ -199,12 +200,19 @@ class AnonymousUser(AnonymousUserMixin):
 login_manager.anonymous_user = AnonymousUser
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
+
 
     @staticmethod
     def generate_fake(count=100):
@@ -221,6 +229,11 @@ class Post(db.Model):
             db.session.add(p)
             db.session.commit()
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
+                        'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target_body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+db.event.listen(Post.body, 'set', Post.on_changed_body)
